885a6268b60af5eb6e00fe10f58db0e355940489
Deepak Negi <deepak99negi@gmail.com>
2025-08-07 21:23:27 +0530

    Changes in Program file
    Changes in Program file

---
+++ LearningCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿Using System;
+
+namespace LearningCSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
