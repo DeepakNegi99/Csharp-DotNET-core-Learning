65dbaf0b74bc857163b10d05ac9341519c73e1f5
Deepak Negi <deepak99negi@gmail.com>
2026-05-09 21:30:31 +0530

    Add C# tutorial projects and exercises
    Add multiple C# learning repositories and materials: DotNetInterviewPractice (50InterviewQuestions - junior/mid topics and examples), LinqTutorialNet6 (exercises, solutions, tests, tutorial examples and data types), and UltimateCSharpMasterclass (coding exercises, examples and additional resources). Remove a few legacy Learning files. This brings a large set of example programs, exercises and unit tests for C#/.NET learning and interview prep.

---
+++ DotNetInterviewPractice/50InterviewQuestionsJunior-main/ClassVsStruct/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+var point = new Point(); //parameterless constructor is always present for structs
+
+Console.ReadKey();
+
+public struct Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x; 
+        Y = y;
+    }
+
+    //this will not work - structs can't have finalizers
+    //~Point()
+    //{
+
+    //}
+
+    //    //before C# 10 this would not compile - struct couldn't have
+    //    //explicit parameterless constuctor
+    //    public Point()
+    //    {
+
+    //    }
+
+    //    //before C# 10 this would not compile - all fields must be
+    //    //assigned in the constructor
+    //    public Point(int x)
+    //    {
+
+    //    }
+}
+
+//This will not work - structs do not support inheritance
+//public struct SpecialPoint : Point
+//{
+
+//}
+
+//this works fine - structs can implement interfaces
+
+public struct SpecialPoint : IComparable<SpecialPoint>
+{
+    public int CompareTo(SpecialPoint other)
+    {
+        throw new NotImplementedException();
+    }
+}
+
